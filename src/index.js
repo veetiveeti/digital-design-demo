a430a13e462e6bb7dc94e7d0ebf9f751010b6741
Veeti <veeti.v.reponen@gmail.com>
2019-11-12 09:50:54 +0200

    JSX and CSS work
    - Worked on JSX and CSS

--- src/index.js
+++ src/index.js
@@ -126,7 +126,6 @@ const MainContent = () => {
     return (
      <section className='description-wrapper'>
         <div className='description-container'>
-        <h2 className='long-description-headline'>Headline for Long Description</h2>
         <p className='long-description-paragraph'>
          Aratan balchoth barrow-downs battle of tumhalad battle-under-stars captain of morgoth dramborleg elves of the falas fang first deep gardner great horn helm hammerhand hild icebay of forochel karningul limlint lossoth ring of water sorcerer of dol guldur tobold hornblower venturers yearbook of tuckborough. Elenya fat goodbody king of nargothrond lobelia bracegirdle rose gamgee tim. 
          Angband battle of the powers boffin citadel of gondor enerdhil ered nimrais fanuidhol gothmog, lord of balrogs great captain hobbiton hill hobbitry-in-arms isen long night lord of beleriand nan dungortheb nick cotton orophin paths of the dead pennies two trees of the valar. 
